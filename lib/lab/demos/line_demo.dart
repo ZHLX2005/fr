3353e8408ba68dd7344df04d9cc7b343665720cc
joke-lx <2196824165@qq.com>
2026-04-06 22:44:47 +0800

    feat(line): route LineDemo to SongSelectPage

--- lib/lab/demos/line_demo.dart
+++ lib/lab/demos/line_demo.dart
@@ -8,3 +8,19 @@ import '../../lab/lab_container.dart';
 void registerLineDemo() {
   demoRegistry.register(LineDemo());
 }
+
+class LineDemo extends DemoPage {
+  @override
+  String get title => '线';
+
+  @override
+  String get description => '线';
+
+  @override
+  bool get preferFullScreen => true;
+
+  @override
+  Widget buildPage(BuildContext context) {
+    return const SongSelectPage();
+  }
+}
