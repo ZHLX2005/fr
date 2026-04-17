4d58aff77a9a1bd22a3a4ca31edd3c90d3999aa2
zhaoliuxue111 <zhaoliuxue111@xiaoduotech.com111>
2026-04-17 23:46:39 +0800

    fix

---
+++ lib/lab/demos/const_pull_depth_switch_demo.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const double kPullRefreshThreshold = 72;
+const double kPullLaunchThreshold = 156;
+const double kPullIndicatorMaxExtent = 196;
+const Duration kPullResetDuration = Duration(milliseconds: 240);
+const Duration kPullActionDuration = Duration(milliseconds: 520);
+
+const Color kPullDemoBackground = Color(0xFFF4F7FB);
+const Color kPullDemoSurface = Colors.white;
+const Color kPullDemoPrimary = Color(0xFF2563EB);
+const Color kPullDemoSecondary = Color(0xFF0EA5A4);
+const Color kPullDemoAccent = Color(0xFFF59E0B);
+const Color kPullDemoText = Color(0xFF0F172A);
+const Color kPullDemoSubtleText = Color(0xFF475569);
+
