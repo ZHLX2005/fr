d4e68f79cb09eca602f2304c00887e869580f9c6
zhaoliuxue <2096343460@qq.com>
2026-05-12 10:02:17 +0800

    delete

--- lib/lab/lab_bootstrap.dart
+++ lib/lab/lab_bootstrap.dart
@@ -20,7 +20,6 @@ import 'demos/localnet_demo.dart';
 import 'demos/my_diary_header_demo.dart';
 import 'demos/network_demo.dart';
 import 'demos/network_env_demo.dart';
-import 'demos/notebook_demo_ai_proto.dart';
 import 'demos/novel_reader_demo.dart';
 import 'demos/overlay_demo.dart';
 import 'demos/qr_demo.dart';
@@ -38,7 +37,6 @@ import 'demos/rive_pendulum_demo.dart';
 // 注册所有 Demo 页面
 void registerAllDemos() {
   registerGridDashboardDemo();
-  registerNotebookDemoAiProto();
   registerClockDemo();
   registerCrashLogDemo();
   registerNetworkDemo();
